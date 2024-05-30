106867c2bdd8e255ca2939d04d867efa3e006e30
Ömer Tokay <inborninnate1@gmail.com>
2024-05-30 10:40:41 +0300

    MVC Model

---
+++ spring-09-MVC-Model/src/main/java/org/cydeo/model/Student.java
@@ -0,0 +1,14 @@
+package org.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
