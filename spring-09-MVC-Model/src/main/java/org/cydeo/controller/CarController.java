3fe3eaa9a81e14b640ddd0c3e6ac9c1ee0bb902d
Ömer Tokay <inborninnate1@gmail.com>
2024-05-30 16:28:45 +0300

    MVC Model Query Parameters & Path Variable

--- spring-09-MVC-Model/src/main/java/org/cydeo/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/org/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package org.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,19 +15,39 @@ public class CarController {
         model.addAttribute("make", make);
         return "car/car-info";
     }
+
     //localhost:8080/info2 (KIA)
     @RequestMapping("/info2")
-    public String carInfo2(@RequestParam(value = "make",required = false,defaultValue = "KIA") String make, Model model) {
+    public String carInfo2(@RequestParam(value = "make", required = false, defaultValue = "KIA") String make, Model model) {
 
         model.addAttribute("make", make);
         return "car/car-info";
     }
-// localhost:8080/info3?make=Honda&year=2015
+
+    // localhost:8080/info3?make=Honda&year=2015
     @RequestMapping("/info3")
-    public String carInfo3(@RequestParam String make,@RequestParam int year, Model model) {
+    public String carInfo3(@RequestParam String make, @RequestParam int year, Model model) {
 
         model.addAttribute("make", make);
         model.addAttribute("year", year);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info/{make}/{year}") //localhost:8080/info/Anadolu/2000
+    public String getCarInfo(@PathVariable String make,@PathVariable int year){
+
+        System.out.println(make);
+        System.out.println(year);
+
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info2/{make}/{year}") //localhost:8080/info/BMW/1934
+    public String getCarInfo2(@PathVariable("make") String car,@PathVariable int year){
+
+        System.out.println(car);
+        System.out.println(year);
+
         return "car/car-info";
     }
 
