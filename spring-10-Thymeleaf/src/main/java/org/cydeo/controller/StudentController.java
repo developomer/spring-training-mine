083b7a49701390be54d514b9cbc205db62354a74
Ömer Tokay <inborninnate1@gmail.com>
2024-05-31 15:19:27 +0300

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/org/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/org/cydeo/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
@@ -21,6 +22,14 @@ public class StudentController {
 
 
         return "student/register";
+    }
+
+     @RequestMapping("/welcome") // localhost:8080/student/welcome?id=3
+    public String welcome( Model model, @RequestParam String name) {
+
+         System.out.println(name);
+
+        return "student/welcome";
     }
 
 }
