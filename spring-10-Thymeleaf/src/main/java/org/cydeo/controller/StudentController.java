e3ab41147c768490f3c4d8850848bb8a952a1f98
Ömer Tokay <inborninnate1@gmail.com>
2024-06-03 11:30:20 +0300

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/org/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/org/cydeo/controller/StudentController.java
@@ -3,14 +3,17 @@ package org.cydeo.controller;
 import org.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register") // localhost:8080/student/register
+    @RequestMapping(value = "/register", method = RequestMethod.GET) // localhost:8080/student/register
+    @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("students", DataGenerator.createStudents());
         return "student/register";
@@ -24,7 +27,8 @@ public class StudentController {
         return "student/register";
     }
 
-     @RequestMapping("/welcome") // localhost:8080/student/welcome?id=3
+    @RequestMapping(value = "/welcome", method = RequestMethod.POST) // localhost:8080/student/welcome?id=3
+    @PostMapping("/welcome")
     public String welcome() {
 
 
