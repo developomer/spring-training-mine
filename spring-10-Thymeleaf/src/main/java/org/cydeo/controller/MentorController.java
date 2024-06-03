f64ae3c291782326e8ec7c78b5a548eb8bc8d70d
Ömer Tokay <inborninnate1@gmail.com>
2024-06-03 12:57:54 +0300

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/org/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/org/cydeo/controller/MentorController.java
@@ -1,8 +1,13 @@
 package org.cydeo.controller;
 
+import org.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -10,8 +15,12 @@ public class MentorController {
 
 
     @GetMapping("/register") // localhost:8080/mentor/register
-    public String register() {
+    public String register(Model model) {
 
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3", "JD4", "JD5", "JD6");
+        model.addAttribute("batchList", batchList);
+
+        model.addAttribute("mentor", new Mentor());
 
         return "mentor/mentor-register";
     }
