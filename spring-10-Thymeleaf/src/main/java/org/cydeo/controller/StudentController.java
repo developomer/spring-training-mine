cf6d4868dd6f98272610099cd284fe9502ffecfb
Ömer Tokay <inborninnate1@gmail.com>
2024-06-03 11:30:59 +0300

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/org/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/org/cydeo/controller/StudentController.java
@@ -6,19 +6,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET) // localhost:8080/student/register
+    //@RequestMapping(value = "/register", method = RequestMethod.GET) // localhost:8080/student/register
     @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("students", DataGenerator.createStudents());
         return "student/register";
     }
 
+    //@RequestMapping(value = "/welcome", method = RequestMethod.POST) // localhost:8080/student/welcome?id=3
+    @PostMapping("/welcome")
+    public String welcome() {
+
+
+        return "student/welcome";
+    }
 
     @RequestMapping("/drop") // localhost:8080/student/drop
     public String drop() {
@@ -27,12 +33,5 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping(value = "/welcome", method = RequestMethod.POST) // localhost:8080/student/welcome?id=3
-    @PostMapping("/welcome")
-    public String welcome() {
-
-
-        return "student/welcome";
-    }
 
 }
